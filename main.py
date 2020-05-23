3d555569da2a0548ca3fcd79a748c75e01ed3f7b
tom <tom.charlesworth1997@gmail.com>
2020-05-23 02:34:00 +0100

    Implement YAML for config

--- main.py
+++ main.py
@@ -8,12 +8,25 @@ import flask
 import os
 import json
 import pandas as pd
+import yaml
+
+config_file = open("config.yaml")
+config = yaml.load(config_file, Loader=yaml.FullLoader)
+
+# YAML CONFIG VARIABLES
+NAME = config['Data']['name']
+HOST_NAME = config['Redis']['hostname']
+PORT = config['Redis']['port']
+DB = config['Redis']['db']
+START_TIME = config['Data']['start_time']
+END_TIME = config['Data']['end_time']
+INTERVALS = config['Data']['intervals']
 
 df_list = dict()
 
 
 def get_data(name):
-    redis_conn = redis.Redis(host='localhost', port=6379, db=2, decode_responses=True)
+    redis_conn = redis.Redis(host=HOST_NAME, port=PORT, db=DB, decode_responses=True)
     key_list = []
     for key in redis_conn.keys():
         if name in key:
@@ -27,7 +40,7 @@ def get_data(name):
 
 def create_graphs(key):
     df = pd.DataFrame(df_list[key], columns=['Reading'])
-    df['Date'] = pd.date_range("00:00:00", "23:59:00", freq="15min").strftime('%H:%M:%S')
+    df['Date'] = pd.date_range(START_TIME, END_TIME, freq=INTERVALS).strftime('%H:%M:%S')
     dummy_plot = go.Scatter(
         x=df.Date,
         y=df['Reading'],
@@ -83,6 +96,5 @@ def init_dash(keys):
 
 
 if __name__ == '__main__':
-    project_name = "GREENWICH"
-    keys = get_data(name=project_name)
+    keys = get_data(name=NAME)
     init_dash(keys)
